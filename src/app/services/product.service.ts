5ec7ee241abe7e6d11c6cf6be2454b8756e6ed3f
Imad-It <itgeek14@gmail.com>
2026-04-17 23:54:40 +0200

    feat: add Product and Category models

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { Product } from '../models/product.model';
 
 @Injectable({
   providedIn: 'root',
@@ -10,7 +11,7 @@ export class ProductService {
 
   private http = inject(HttpClient);
 
-  getProducts(): Observable<any[]> {
-    return this.http.get<any[]>(this.apiUrl);
+  getProducts(): Observable<Product[]> {
+    return this.http.get<Product[]>(this.apiUrl);
   }
 }
