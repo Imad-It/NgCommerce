91b6266496cc19d36107aa218e8ce3d344779594
Imad-It <itgeek14@gmail.com>
2026-04-24 22:28:04 +0200

    refactor(core): move navbar and sidebar into layout

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,9 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { NavbarComponent } from './components/navbar/navbar.component';
-import { SidebarCompoent } from './components/sidebar/sidebar.compoent';
+import { SidebarCompoent } from './core/layout/sidebar/sidebar.compoent';
 import { ProductListComponent } from './components/products/product-list/product-list.component';
 import { CartPageComponent } from './components/cart/cart-page/cart-page.component';
+import { NavbarComponent } from './core/layout/navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
