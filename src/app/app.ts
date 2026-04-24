8d2f2062b22b12eaabc8af83ec05e1731ac49350
Imad-It <itgeek14@gmail.com>
2026-04-24 23:49:09 +0200

    refactor(categories): create categories folder and move category interface

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,9 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { SidebarCompoent } from './core/layout/sidebar/sidebar.compoent';
-import { ProductListComponent } from './components/products/product-list/product-list.component';
 import { CartPageComponent } from './features/cart/cart-page/cart-page.component';
 import { NavbarComponent } from './core/layout/navbar/navbar.component';
+import { ProductListComponent } from './features/products/product-list/product-list.component';
 
 @Component({
   selector: 'app-root',
