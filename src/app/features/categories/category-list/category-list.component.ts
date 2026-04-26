acd8b71e848c2544a681587250d29b6fd4e2669c
Imad-It <itgeek14@gmail.com>
2026-04-27 01:33:03 +0200

    feat(categories): add CategoryCardComponent

--- src/app/features/categories/category-list/category-list.component.ts
+++ src/app/features/categories/category-list/category-list.component.ts
@@ -1,10 +1,11 @@
 import { Component, inject } from '@angular/core';
 import { CategoryService } from '../services/category.service';
 import { toSignal } from '@angular/core/rxjs-interop';
+import { CategoryCardComponent } from '../category-card/category-card.component';
 
 @Component({
   selector: 'app-category-list',
-  imports: [],
+  imports: [CategoryCardComponent],
   templateUrl: './category-list.component.html',
   styleUrl: './category-list.component.css',
 })
