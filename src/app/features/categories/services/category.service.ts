fe327a72e25623cb1b8ec2f51d9c33c3a1c08f87
Imad-It <itgeek14@gmail.com>
2026-05-01 20:17:37 +0200

    refactor(category-service): add loading indicator to getCategories()

--- src/app/features/categories/services/category.service.ts
+++ src/app/features/categories/services/category.service.ts
@@ -1,8 +1,9 @@
 import { inject, Injectable } from '@angular/core';
 import { environment } from '../../../../environments/environment';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { finalize, Observable } from 'rxjs';
 import { Category } from '../models/category.model';
+import { LoadingService } from '../../../core/services/loading.service';
 
 @Injectable({
   providedIn: 'root',
@@ -11,8 +12,12 @@ export class CategoryService {
   private readonly baseUrl = environment.apiUrl;
 
   private http = inject(HttpClient);
+  private loadingService = inject(LoadingService);
 
   getCategories(): Observable<Category[]> {
-    return this.http.get<Category[]>(`${this.baseUrl}/categories`);
+    this.loadingService.setLoading(true);
+    return this.http
+      .get<Category[]>(`${this.baseUrl}/categories`)
+      .pipe(finalize(() => this.loadingService.setLoading(false)));
   }
 }
