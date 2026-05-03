0c33585b6da4e4f624c11001fed8910e1c3ced76
Imad-It <itgeek14@gmail.com>
2026-05-03 21:02:21 +0200

    feat(sidebar): load categories dynamically from API

--- src/app/core/layout/sidebar/sidebar.compoent.ts
+++ src/app/core/layout/sidebar/sidebar.compoent.ts
@@ -1,6 +1,9 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { toSignal } from '@angular/core/rxjs-interop';
 import { RouterLink, RouterModule } from '@angular/router';
+import { CategoryService } from '../../../features/categories/services/category.service';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-sidebar',
@@ -8,4 +11,9 @@ import { RouterLink, RouterModule } from '@angular/router';
   templateUrl: './sidebar.compoent.html',
   styleUrl: './sidebar.compoent.css',
 })
-export class SidebarCompoent {}
+export class SidebarCompoent {
+  categoryService = inject(CategoryService);
+  categories = toSignal(
+    this.categoryService.getCategories().pipe(map((categories) => categories.slice(0, 4))),
+  );
+}
