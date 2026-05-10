8d791b55ae44907843443f57abaa690fd0c86625
Imad-It <itgeek14@gmail.com>
2026-05-10 19:27:08 +0200

    feat: add initial product-filter component structure

---
+++ src/app/features/products/product-filter/product-filter.component.ts
@@ -0,0 +1,34 @@
+import { Component, inject } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { FaIconComponent } from '@fortawesome/angular-fontawesome';
+import { faArrowRotateRight, faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
+import { NgxSliderModule, Options } from '@angular-slider/ngx-slider';
+import { CategoryService } from '../../categories/services/category.service';
+import { toSignal } from '@angular/core/rxjs-interop';
+import { Category } from '../../categories/models/category.model';
+
+@Component({
+  selector: 'app-product-filter',
+  imports: [FaIconComponent, NgxSliderModule, FormsModule],
+  templateUrl: './product-filter.component.html',
+  styleUrl: './product-filter.component.css',
+})
+export class ProductFilterComponent {
+  private categoryService = inject(CategoryService);
+  categories = toSignal(this.categoryService.getCategories(), { initialValue: [] as Category[] });
+  faMagnifyingGlass = faMagnifyingGlass;
+  faArrowRotateRight = faArrowRotateRight;
+  minPrice = 50;
+  maxPrice = 300;
+
+  options: Options = {
+    floor: 0,
+    ceil: 1000,
+    step: 10,
+    translate: (value: number): string => value + ' €',
+  };
+  onSelectChange(event: Event) {
+    const selectElement = event.target as HTMLSelectElement;
+    console.log('Selected value:', selectElement.value);
+  }
+}
