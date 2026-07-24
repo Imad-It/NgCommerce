9fa7ca9b457698250b19d00a91234ed86ad332c3
Imad-It <itgeek14@gmail.com>
2026-07-24 23:41:47 +0200

    feat(auth): integrate Angular Form Signals into login form

--- src/app/features/auth/components/login-form/login-form.component.ts
+++ src/app/features/auth/components/login-form/login-form.component.ts
@@ -1,11 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
+import { email, form, required, FormField } from '@angular/forms/signals';
+import { AuthService } from '../../../../core/auth/services/auth.service';
+import * as formUtils from '../../../../shared/utils/form.util';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-login-form',
-  imports: [],
+  imports: [CommonModule, FormField],
   templateUrl: './login-form.component.html',
   styleUrl: './login-form.component.css',
 })
 export class LoginFormComponent {
+  private authService = inject(AuthService);
+  loginRequestModel = signal({ email: '', password: '' });
+  readonly formUtils = formUtils;
 
+  loginRequestForm = form(this.loginRequestModel, (schema) => {
+    required(schema.email, { message: 'Name is required.' });
+    email(schema.email, { message: 'Please enter a valid email address.' });
+    required(schema.password, { message: 'Name is required.' });
+  });
+
+  login() {
+    const credential = {
+      email: this.loginRequestForm.email().value(),
+      password: this.loginRequestForm.password().value(),
+    };
+    console.log('#######', credential);
+    this.authService.login(credential);
+  }
 }
